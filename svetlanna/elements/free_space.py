b720eaf8353bde42b583122127cb99b75be1f815
djiboshin <64642744+djiboshin@users.noreply.github.com>
2024-12-13 05:14:56 +0300

    code review response

--- svetlanna/elements/free_space.py
+++ svetlanna/elements/free_space.py
@@ -44,99 +44,92 @@ class FreeSpace(Element):
         self._w_index = self.simulation_parameters.axes.index('W')
         self._h_index = self.simulation_parameters.axes.index('H')
 
-        # TODO: Why we need `_` before a variable name? Anyway it is not an attribute of the class
-        # TODO: Is there some convention for `_` and `__` in a variable name?
-        _x_linear = self.simulation_parameters.axes.W
-        _y_linear = self.simulation_parameters.axes.H
+        x_linear = self.simulation_parameters.axes.W
+        y_linear = self.simulation_parameters.axes.H
 
-        x_nodes = _x_linear.shape[0]
-        y_nodes = _y_linear.shape[0]
+        x_nodes = x_linear.shape[0]
+        y_nodes = y_linear.shape[0]
 
-        # spatial frequencies calculation
-        dx = (_x_linear[1] - _x_linear[0]) if x_nodes > 1 else 1.
-        dy = (_y_linear[1] - _y_linear[0]) if y_nodes > 1 else 1.
+        # Compute spatial grid spacing
+        dx = (x_linear[1] - x_linear[0]) if x_nodes > 1 else 1.
+        dy = (y_linear[1] - y_linear[0]) if y_nodes > 1 else 1.
 
-        kx_linear = 2 * torch.pi * torch.fft.fftfreq(
-            x_nodes,
-            dx,
-            device=device
-        )
-        ky_linear = 2 * torch.pi * torch.fft.fftfreq(
-            y_nodes,
-            dy,
-            device=device
-        )
+        # Compute wave vectors
+        kx_linear = 2 * torch.pi * torch.fft.fftfreq(x_nodes, dx, device=device)
+        ky_linear = 2 * torch.pi * torch.fft.fftfreq(y_nodes, dy, device=device)
 
-        # spatial frequencies mesh
-        kx_grid = kx_linear[None, :]
-        ky_grid = ky_linear[:, None]
+        # Compute wave vectors grids
+        kx_grid = kx_linear[None, :]  # shape: (1, 'W')
+        ky_grid = ky_linear[:, None]  # shape: ('H', 1)
 
         # Calculate (kx^2+ky^2) / k^2 relation
-        # 1) float or torch.Tensor of 'wavelength' axis size
+        # 1) Calculate wave vector of shape ('wavelength') or ()
         k = 2 * torch.pi / self.simulation_parameters.axes.wavelength
-        # 2) calculate (kx^2+ky^2) tensor of size ('H', 'W')
-        kx2ky2 = kx_grid ** 2 + ky_grid ** 2
-        # 3) relation
-        relation, relation_axes = tensor_dot(
-            1 / (k ** 2),
-            kx2ky2,
-            'wavelength',
-            ('H', 'W')
-        )
 
-        # TODO: What is that? Is this code for some future features?
-        # TODO: Must not a `use_filter` be an attribute of the Class?
-        # low pass filter, (kx^2 + ky^2) <= k^2
+        # 2) Calculate (kx^2+ky^2) tensor
+        kx2ky2 = kx_grid ** 2 + ky_grid ** 2  # shape: ('H', 'W')
+
+        # 3) Calculate (kx^2+ky^2) / k^2
+        relation, relation_axes = tensor_dot(
+            a=1 / (k ** 2),
+            b=kx2ky2,
+            a_axis='wavelength',
+            b_axis=('H', 'W')
+        )  # shape: ('wavelength', 'H', 'W') or ('H', 'W') depending on k shape
+
+        # TODO: Remove legacy filter
+        use_legacy_filter = False
+
+        # Legacy low pass filter, (kx^2+ky^2) / k^2 <= 1
+        # The filter removes contribution of evanescent waves
         self._low_pass_filter: torch.Tensor | int  # <- Registering Buffer for _low_pass_filter
-        use_filter = False
-        if use_filter:
+        if use_legacy_filter:
             # TODO: Shouldn't the 88'th string be here?
+            condition = (relation <= 1)  # calculate the low pass filter condition
+            condition = condition.to(kx_grid)  # cast bool to float
             self.register_buffer(
-                '_low_pass_filter',
-                (relation <= 1).to(kx_grid),  # TODO: I did not understand this condition...
-                persistent=False
+                '_low_pass_filter', condition, persistent=False
             )
         else:
             self._low_pass_filter = 1
 
-        self._wave_number: torch.Tensor
+        # Reshape wave vector for further calculations
+        wave_number = k[..., None, None]  # shape: ('wavelength', 1, 1) or (1, 1)
+        self._wave_number: torch.Tensor  # <- Registering Buffer for _wave_number
         self.register_buffer(
-            '_wave_number',
-            k[..., None, None],
-            persistent=False
-        )  # TODO: Does a Buffer works even if `k` is not a Class attribute (not self.k)?
+            '_wave_number', wave_number, persistent=False
+        )
 
         self._calc_axes = relation_axes  # axes tuple used during calculations
-        _wave_number_x2y2 = self._low_pass_filter * kx2ky2
 
-        # kz
-        if use_filter:  # TODO: what the difference between `if` and `else`?
-            # kz
-            #    = sqrt(k^2 - (kx^2 + ky^2)), if (kx^2 + ky^2) <= k^2;
-            #    = |k| otherwise.
+        # Calculate kz
+        if use_legacy_filter:
+            # kz = sqrt(k^2 - (kx^2 + ky^2)), if (kx^2 + ky^2) / k^2 <= 1
+            #    or
+            # kz = |k| otherwise
             wave_number_z = torch.sqrt(
-                self._wave_number ** 2 - _wave_number_x2y2  # TODO: `+ 0j`?
+                self._wave_number ** 2 - self._low_pass_filter * kx2ky2
             )
-        else:  #
+        else:
+            # kz = sqrt(k^2 - (kx^2 + ky^2))
             wave_number_z = torch.sqrt(
-                self._wave_number ** 2 - _wave_number_x2y2 + 0j
-            )
+                self._wave_number ** 2 - kx2ky2 + 0j
+            )  # 0j is required to convert argument to complex
 
         self._wave_number_z: torch.Tensor  # <- Registering Buffer for _wave_number_z
         self.register_buffer(
-            '_wave_number_z',
-            wave_number_z,
-            persistent=False
+            '_wave_number_z', wave_number_z, persistent=False
         )
 
-        # kz taylored, used by Fresnel approximation
-        # TODO: Do we need this if `method` is not equal to 'fresnel'?
-        self._wave_number_z_eff_fresnel: torch.Tensor  # <- Registering Buffer for _wave_number_z_eff_fresnel
+        # Calculate kz taylored, used by Fresnel approximation
+        wave_number_z_eff_fresnel = - 0.5 * kx2ky2 / self._wave_number
+
+        self._wave_number_z_eff_fresnel: torch.Tensor
+        #  ^- Registering Buffer for _wave_number_z_eff_fresnel
         self.register_buffer(
-            '_wave_number_z_eff_fresnel',
-            - 0.5 * _wave_number_x2y2 / self._wave_number,
-            persistent=False
+            '_wave_number_z_eff_fresnel', wave_number_z_eff_fresnel, persistent=False
         )
+
         # TODO: Maybe add a separate method to define all necessary Buffers?
 
     def impulse_response_angular_spectrum(self) -> torch.Tensor:
@@ -149,7 +142,7 @@ class FreeSpace(Element):
         """
 
         # Fourier image of impulse response function,
-        # 0 if k^2 < (kx^2 + ky^2) [if use_filter]
+        # 0 if k^2 < (kx^2 + ky^2) [if use_legacy_filter]
         # TODO: there is still no information in docstrings about a filter:(
         return self._low_pass_filter * torch.exp(
             (1j * self.distance) * self._wave_number_z
@@ -165,6 +158,7 @@ class FreeSpace(Element):
         """
 
         # Fourier image of impulse response function
+        # 0 if k^2 < (kx^2 + ky^2) [if use_legacy_filter]
         return self._low_pass_filter * torch.exp(
             (1j * self.distance) * self._wave_number_z_eff_fresnel
         ) * torch.exp(
@@ -172,7 +166,18 @@ class FreeSpace(Element):
         )
 
     def _impulse_response(self, tol: float = 1e-3) -> torch.Tensor:
-        # TODO: No docstring!
+        """Calculate the impulse response function based on selected method
+
+        Parameters
+        ----------
+        tol : float, optional
+            tolerance for auto method, by default 1e-3
+
+        Returns
+        -------
+        torch.Tensor
+            The impulse response function
+        """
 
         if self.method == 'AS':
             return self.impulse_response_angular_spectrum()
@@ -184,8 +189,7 @@ class FreeSpace(Element):
         # TODO: Didn't we plan to exclude `auto` method at all?
         elif self.method == 'auto':
 
-            # TODO: Somewhere we use `** 2`, somewhere `torch.pow(_, 2)`
-            radius_squared = torch.pow(self._x_grid, 2) + torch.pow(self._y_grid, 2)
+            radius_squared = self._x_grid**2 + self._y_grid**2
 
             # criterion for Fresnel approximation
             fresnel_criterion = (
@@ -203,7 +207,7 @@ class FreeSpace(Element):
 
         raise ValueError("Unknown forward propagation method")
 
-    # TODO: ask for tol parameter
+    # TODO: ask for tol parameter, maybe move it to init?
     def forward(
         self,
         input_field: Wavefront
@@ -214,8 +218,6 @@ class FreeSpace(Element):
         ----------
         input_field : Wavefront
             Field before propagation in free space
-        tol : float, optional
-            tolerance for Fresnel approximation, by default 1e-3
 
         Returns
         -------
@@ -237,12 +239,12 @@ class FreeSpace(Element):
 
         # Fourier image of output field
         output_field_fft, _ = tensor_dot(
-            input_field_fft,  # TODO: It's really hard to track dimensions... Maybe add some comments for some tensors?
-            impulse_response_fft,
-            self.simulation_parameters.axes.names,  # TODO: Will it be better to use named arguments here?
-            self._calc_axes,
-            preserve_a_axis=True
-        )
+            a=input_field_fft,  # example shape: (5, 'wavelength', 1, 'H', 'W')
+            b=impulse_response_fft,  # example shape: ('wavelength', 'H', 'W')
+            a_axis=self.simulation_parameters.axes.names,
+            b_axis=self._calc_axes,
+            preserve_a_axis=True  # check that the output has the input shape
+        )  # example output shape: (5, 'wavelength', 1, 'H', 'W')
 
         output_field = torch.fft.ifft2(
             output_field_fft,
@@ -253,18 +255,18 @@ class FreeSpace(Element):
 
     def reverse(self, transmission_field: torch.Tensor) -> Wavefront:
         # TODO: Check the description...
-        """Method that calculates the field after propagating in the free space
-        in back propagation
+        """Calculate the field after it propagates in the free space
+        in the backward direction.
 
         Parameters
         ----------
         transmission_field : torch.Tensor
-            Field before propagation in free space in back propagation  # TODO: Check!
+            Field to be propagated in the backward direction
 
         Returns
         -------
         torch.Tensor
-            Field after propagation in free space in back propagation
+            Propagated in the backward direction field
         """
 
         transmission_field_fft = torch.fft.fft2(
@@ -276,12 +278,12 @@ class FreeSpace(Element):
 
         # Fourier image of output field
         incident_field_fft, _ = tensor_dot(
-            transmission_field_fft,
-            impulse_response_fft,
-            self.simulation_parameters.axes.names,  # TODO: Named arguments
-            self._calc_axes,
-            preserve_a_axis=True
-        )
+            a=transmission_field_fft,  # example shape: (5, 'wavelength', 1, 'H', 'W')
+            b=impulse_response_fft,  # example shape: ('wavelength', 'H', 'W')
+            a_axis=self.simulation_parameters.axes.names,
+            b_axis=self._calc_axes,
+            preserve_a_axis=True  # check that the output has the input shape
+        )  # example output shape: (5, 'wavelength', 1, 'H', 'W')
 
         incident_field = torch.fft.ifft2(
             incident_field_fft,
