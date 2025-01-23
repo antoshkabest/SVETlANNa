94debad3104df2d4cae1020e0c2bdd949999ac2b
Den4S <denis1123si@gmail.com>
2025-01-23 21:13:03 +0300

    attenuation coefficients added to reservoir + some fixes

--- svetlanna/recurrent_reservoir.py
+++ svetlanna/recurrent_reservoir.py
@@ -17,16 +17,28 @@ class RecurrentReservoir(nn.Module):
         simulation_parameters: SimulationParameters,
         forward_elements: Iterable[Element],
         backward_elements: Iterable[Element],
+        attenuation_forward: float = 0,
+        attenuation_backward: float = 0,
         init_hidden: Wavefront | None = None,
         device: str | torch.device = torch.get_default_device(),
     ) -> None:
         """
         Parameters
         ----------
+        simulation_parameters : SimulationParameters
+            Simulation parameters for a task.
         forward_elements : Iterable[Element]
-            A set of optical elements...
+            A set of optical elements a sum of input and hidden wavefront propagates through.
         backward_elements : Iterable[Element]
             A set of optical elements for a feedback (to get a hidden state).
+            An output wavefront (after forward elements) goes through backward elements and becomes a new hidden state.
+        attenuation_forward, attenuation_backward: float
+            Coefficients which characterize a part of energy that dissipates while forward and backward propagation.
+            Values lie between 0 and 1.
+        init_hidden : Wavefront | None
+            A n initial hidden wavefront ("a cold state").
+        device : torch.device
+            A device for calculations.
         """
         super().__init__()
 
@@ -41,25 +53,31 @@ class RecurrentReservoir(nn.Module):
         self.forward_elements = [el.to(self.__device) for el in forward_elements]
         self.backward_elements = [el.to(self.__device) for el in backward_elements]
 
+        # attenuation constants
+        self.attenuation_forward = attenuation_forward
+        self.attenuation_backward = attenuation_backward
+
+        # a hidden state initialization
         if init_hidden is None:
-            self.hidden_state = self.reset_hidden
+            self.hidden_state = None
+            self.reset_hidden()
         else:
             self.hidden_state = init_hidden.to(self.__device)
 
     def reset_hidden(self):
         """
-        Resets a "hidden" wavefront.
+        Resets a "hidden" wavefront to zeros.
 
         Returns
         -------
         Wavefront
             A zero wavefront as an initial "hidden" wavefront.
         """
-        return Wavefront(
+        self.hidden_state = Wavefront(
             torch.zeros(
-                # TODO: right way to get a wavefront size?
+                # TODO: right way to get a complete wavefront size?
                 size=self.simulation_parameters.axes_size(
-                    self.simulation_parameters.axes.__dir__()
+                    axs=('H', 'W')
                 )
             )
         ).to(self.__device)
@@ -82,12 +100,14 @@ class RecurrentReservoir(nn.Module):
         output_wavefront = input_wavefront + self.hidden_state
         for el in self.forward_elements:
             output_wavefront = el.forward(output_wavefront)
+        output_wavefront = output_wavefront * (1 - self.attenuation_forward) ** (1 / 2)
 
         # get a next hidden by propagation of the output
         hidden_state = output_wavefront
         for el in self.backward_elements:
             hidden_state = el.forward(hidden_state)
-        self.hidden_state = hidden_state  # update hidden state
+        # update hidden state
+        self.hidden_state = hidden_state * (1 - self.attenuation_backward) ** (1 / 2)
 
         return output_wavefront
 
